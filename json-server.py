dc6a434baf3155b102b4337f60070a6c34ed590a
Kyle Mims <kylemims@gmail.com>
2025-06-18 06:54:04 -0500

    Refactored the do_GET funtion to route all entities, exluding 'DemotionQueue'. Removed the 'response_body' variable to simplify each if/elif condition statement

--- json-server.py
+++ json-server.py
@@ -29,23 +29,80 @@ class JSONServer(HandleRequests):
         url = self.parse_url(self.path)
 
         if url["requested_resource"] == "users":
-            import sqlite3
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_user(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_users(), status.HTTP_200_SUCCESS.value)
 
-            with sqlite3.connect("./db.sqlite3") as conn:
-                conn.row_factory = sqlite3.Row
-                db_cursor = conn.cursor()
+        elif url["requested_resource"] == "subscriptions":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_subscription(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_subscriptions(), status.HTTP_200_SUCCESS.value)
 
-                db_cursor.execute("SELECT id, username, email FROM Users")
+        elif url["requested_resource"] == "posts":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_post(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_posts(), status.HTTP_200_SUCCESS.value)
 
-                users = db_cursor.fetchall()
-                user_list = [dict(u) for u in users]
+        elif url["requested_resource"] == "comments":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_comment(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_comments(), status.HTTP_200_SUCCESS.value)
 
-            return self.response(json.dumps(user_list), status.HTTP_200_SUCCESS.value)
+        elif url["requested_resource"] == "reactions":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_reaction(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_reactions(), status.HTTP_200_SUCCESS.value)
 
-        return self.response(
-            json.dumps({"message": "Not Implemented"}),
-            status.HTTP_501_SERVER_ERROR.value,
-        )
+        elif url["requested_resource"] == "reactions":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_reaction(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_reactions(), status.HTTP_200_SUCCESS.value)
+
+        elif url["requested_resource"] == "postReactions":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_postReaction(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_postReactions(), status.HTTP_200_SUCCESS.value)
+
+        elif url["requested_resource"] == "tags":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_tag(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_tags(), status.HTTP_200_SUCCESS.value)
+
+        elif url["requested_resource"] == "postTags":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_postTag(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_postTags(), status.HTTP_200_SUCCESS.value)
+
+        elif url["requested_resource"] == "categories":
+            if url["pk"] != 0:
+                return self.response(
+                    retrieve_category(url["pk"]), status.HTTP_200_SUCCESS.value
+                )
+            return self.response(list_categories(), status.HTTP_200_SUCCESS.value)
+
+        else:
+            return self.response(
+                json.dumps({"message": "Not Implemented"}),
+                status.HTTP_501_SERVER_ERROR.value,
+            )
 
 
 def main():
@@ -56,3 +113,22 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# import sqlite3
+
+#             with sqlite3.connect("./db.sqlite3") as conn:
+#                 conn.row_factory = sqlite3.Row
+#                 db_cursor = conn.cursor()
+
+#                 db_cursor.execute("SELECT id, username, email FROM Users")
+
+#                 users = db_cursor.fetchall()
+#                 user_list = [dict(u) for u in users]
+
+#             return self.response(json.dumps(user_list), status.HTTP_200_SUCCESS.value)
+
+#         return self.response(
+#             json.dumps({"message": "Not Implemented"}),
+#             status.HTTP_501_SERVER_ERROR.value,
+#         )
