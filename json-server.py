12282e07f91eb9297f4a42ab36590c0869842082
Jake Littleton <jakelittleton30@gmail.com>
2025-06-23 15:47:05 -0500

    edit and delete tags completed

--- json-server.py
+++ json-server.py
@@ -4,7 +4,7 @@ import json
 from views.user import login_user, create_user
 from views import list_subscriptions, retrieve_subscription
 from views import list_comments, retrieve_comment
-from views import list_tags, retrieve_tags, create_tag
+from views import list_tags, retrieve_tags, create_tag, update_tag, delete_tag
 from views import list_categories, retrieve_category
 from views import list_postTags, retrieve_postTag
 from views import list_postReactions, retrieve_postReaction
@@ -123,6 +123,41 @@ class JSONServer(HandleRequests):
                 json.dumps({"message": "Not Implemented"}),
                 status.HTTP_500_SERVER_ERROR.value,
             )
+    
+    def do_PUT(self):
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+        content_len = int(self.headers.get('content-length', 0))
+        request_body = self.rfile.read(content_len)
+        request_body = json.loads(request_body)
+
+        if url["requested_resource"] == "tags":
+            if pk != 0:
+                successfully_updated = update_tag(pk, request_body)
+                if successfully_updated:
+                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                else:
+                    return self.response(
+                        json.dumps({"message": "Tag not found or not updated"}),
+                        status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+            else:
+                return self.response(
+                        json.dumps({"message": "Requested resource not found"}),
+                        status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+                    )
+    
+    def do_DELETE(self):
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+
+        if url["requested_resource"] == "tags":
+            if pk != 0:
+                successfully_deleted = delete_tag(pk)
+                if successfully_deleted:
+                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+
+                return self.response("Requested resource not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+                
 
 
 def main():
