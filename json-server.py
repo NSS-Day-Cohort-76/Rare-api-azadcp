1d874b035c0c0cccc09bb5bc2bc33a19959a81b5
David West <davidpwest1220@gmail.com>
2025-06-25 11:40:05 -0500

    Pulling new updates from develop

--- json-server.py
+++ json-server.py
@@ -5,11 +5,7 @@ from views.user import login_user, create_user
 from views import list_subscriptions, retrieve_subscription
 from views import list_comments, retrieve_comment
 from views import list_tags, retrieve_tags, create_tag, update_tag, delete_tag
-<<<<<<< HEAD
 from views import list_categories, retrieve_category, create_category, delete_category
-=======
-from views import list_categories, retrieve_category, create_category
->>>>>>> develop
 from views import list_postTags, retrieve_postTag
 from views import list_postReactions, retrieve_postReaction
 from views import list_reactions, retrieve_reaction
